ed9af494df866cbdaeb8970bdbf93183293103b6
Inthiyaz <skinthiyaz9581@gmail.com>
2023-07-28 18:03:20 +0530

    updated_code

--- src/API.js
+++ src/API.js
@@ -3,7 +3,12 @@ export const CARDS_IMG_URL =
   "https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/";
 
 export const BASE_URL =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&page_type=DESKTOP_WEB_LISTING";
+// "https://www.swiggy.com/mapi/homepage/getCards?lat=16.2893144&lng=80.4604643";
+"https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING";
+  // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.7040592&lng=77.10249019999999&page_type=DESKTOP_WEB_LISTING";
+
+  //https://www.swiggy.com/dapi/restaurants/list/v5?lat=28.6126255&lng=77.04108959999999&page_type=DESKTOP_WEB_LISTING
 // export default { HEADER_LOGO, CARDS_IMG_URL };
+export const TEST_URL="https://res.cloudinary.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_508,h_320,c_fill/rng/md/carousel/production/d580a6e708996715971aa417aa623683";
 
 export const CORS_PROXY = "https://cors-anywhere.herokuapp.com/";
--- src/components/Offers.js
+++ src/components/Offers.js
@@ -1,23 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./Offers.css";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faArrowLeft, faArrowRight } from '@fortawesome/free-solid-svg-icons'; // Import the necessary icons
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
+import { CARDS_IMG_URL, BASE_URL, CORS_PROXY,TEST_URL } from "../API";
 
 const Offers = () => {
-    return ( 
+    const [carouselData, setCarouselData] = useState([]);
+    const [isLoading, setIsLoading] = useState(true);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch(CORS_PROXY+BASE_URL);
+                const data = await response.json();
+
+                if (data.data && Array.isArray(data.data.cards)) {
+                    setCarouselData((data.data.cards));
+                    console.log("caro" + (data.data.cards));
+                } else {
+                    console.error("Invalid data format:", data);
+                }
+            } catch (error) {
+                console.error("Error fetching carousel data:", error);
+            } finally {
+                setIsLoading(false); // Set loading to false after fetching, whether successful or not
+            }
+        };
+        fetchData();
+    }, []);
+
+    if (isLoading) {
+        return <div>Loading...</div>; // You can replace this with a loading spinner or any other loading UI
+    }
+
+    return (
         <div className="offercontainer">
             <div className="offertop">
                 <h2>Best offers for you</h2>
                 <div className="arrow">
-                    <a href="#"><FontAwesomeIcon icon={faArrowLeft} /></a> 
-                    <a href="#"><FontAwesomeIcon icon={faArrowRight} /></a> 
+                    <a href="#">
+                        <FontAwesomeIcon icon={faArrowLeft} />
+                    </a>
+                    <a href="#">
+                        <FontAwesomeIcon icon={faArrowRight} />
+                    </a>
                 </div>
             </div>
             <div className="imagenav">
-                {/* Add your content here */}
+                {/* Map carouselData to render carousel items */}
+                {carouselData.map((card, index) => ( // Use index as the key for the outer div
+                   <div key={index}>
+                   {card.card.card.imageGridCards &&
+                     card.card.card.imageGridCards.info &&
+                     card.card.card.imageGridCards.info.map((imageInfo) => {
+                       const imageUrl = `${CARDS_IMG_URL}${imageInfo.imageId}`;
+                       console.log("Image URL:",card.card.card.imageGridCards); // Add this line to check the image URLs
+                       return (
+                         <img
+                           key={imageInfo.id}
+                           src={imageUrl}
+                           alt={`Image ${imageInfo.id}`}
+                         />
+                       );
+                     })}
+                 </div>
+                ))}
             </div>
         </div>
     );
-}
- 
-export default Offers; 
+};
+
+
+export default Offers;
